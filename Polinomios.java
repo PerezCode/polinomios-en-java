d29c845c5834d6c156b600324db1f47d91bf102a
JuanJMesa <mesamjuanj@gmail.com>
2019-09-19 18:33:52 -0500

    Evaluar completado.

--- Polinomios.java
+++ Polinomios.java
@@ -6,16 +6,17 @@ public class Polinomios {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        //String polinomioEnString1 = "5x^5+x^2+x-3";
+        String polinomioEnString1 = "5x^5+x^2+x-3";
         String polinomioEnString2 = "x^3+x+8";
-        //Polinomio myPolinomio1 = new Polinomio(polinomioEnString1);
+        Polinomio myPolinomio1 = new Polinomio(polinomioEnString1);
         Polinomio myPolinomio2 = new Polinomio(polinomioEnString2);
         
         for (int i = 0; i <= myPolinomio2.getDatosUtiles(); i++) {
             
             System.out.print("| " + myPolinomio2.getVector()[i] +" |");
         }
-        
-        
+        myPolinomio1.setEvaluar();
+		myPolinomio2.setEvaluar();
     }
+    
 }
